0d0c2382b92072d4c912f5f4dd05bd93bf37d38d
mrpatlov <34523630+mrpatlov@users.noreply.github.com>
2017-12-21 15:23:20 -0500

    Add files via upload

---
+++ PSDetect.py
@@ -0,0 +1,46 @@
+# Author Michael Patlovich
+
+from scapy.all import *
+import os
+import sys
+import argparse
+import socket
+import select
+import logging
+import signal #To kill the programs nicely
+import time
+
+offendersip = []
+offenderstime = []
+offenderstime1 = []
+
+def action(packet):
+	start = time.time()
+	srcip = packet[0][1].src
+	position = -1
+	leng = 0
+	while leng < len(offendersip):
+		if str(srcip) == str(offendersip[leng]):
+			position = leng
+			break
+		leng += 1
+	if position >= 0:
+		offenderstime[position] += 1
+		offenderstime1[position] = time.time() - offenderstime1[position]
+		if offenderstime[position] >= 15 and offenderstime1[position] < 5:
+			offenderstime[position] = 0
+			return 'Scanner detected. The scanner originated from host {}'.format(packet[0][1].src)
+	elif position == -1:
+		offendersip.extend([packet[0][1].src])
+		offenderstime.extend([0])
+		offenderstime1.extend([time.time() - start])
+	
+	
+	
+
+print sniff(filter="ip", prn=action)
+
+
+
+
+
---
+++ PortScanToo.py
@@ -0,0 +1,41 @@
+# Author Michael Patlovich
+
+import os
+import sys
+import argparse
+import socket
+import select
+import logging
+import signal #To kill the programs nicely
+import time
+
+s = sys.argv[1]
+numports = 0
+print "Scanning ports"
+print "_" * 60
+start = time.time()
+try:
+	for port in range(1,65535):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		r = sock.connect_ex((s,port))
+		time.sleep(.5)
+		if r == 0:
+			print "Port {} is OPEN and is used for {}".format(port, socket.getservbyport(port))
+			numports += 1
+		sock.close()
+except socket.error:
+	print "Couldnt connect to the server"
+	sys.exit()
+	
+
+print "Scan finished!"
+
+
+print "_" * 60
+print "{} port(s) found".format(numports)
+end = time.time()
+print "{} seconds elapsed".format(end-start)
+print "{} ports per second".format(65535/(end-start))
+print "_"*60
+print "Terminating normally"
+
